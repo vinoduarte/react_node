dd812e67b0510b8dc987c7258239fdf9cfe1acd5
Vinícius D <vinoduarte11@gmail.com>
2023-12-27 20:43:54 -0300

    installed dotenv and pm2

--- api-movies/src/server.js
+++ api-movies/src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config")
 require("express-async-errors");
 const express = require("express");
 const routes = require("./routes");
@@ -28,7 +29,7 @@ app.use((error, request, response, next) => {
   });
 });
 
-const PORT = 9999;
+const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
   console.log(`O servidor está usando a porta ${PORT}`);
